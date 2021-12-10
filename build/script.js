bba7f17c2a7035900aeaacf75ad77fc27653d307
Iurie <iuriemisinciuc18@gmail.com>
2021-12-10 16:24:08 +0200

    Project starts

---
+++ build/script.js
@@ -0,0 +1,38 @@
+const tswiper = new Swiper(".swiper", {
+  grabCursor: true,
+  initialSlide: 4,
+  slidesPerView: "auto",
+  loop: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+  },
+});
+
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 4,
+  slidesPerGroup: 8,
+  grid: {
+    rows: 2,
+    fill: "row",
+  },
+
+  spaceBetween: 30,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
+
+var swiperService = new Swiper(".service-swipp", {
+  slidesPerView: 2,
+  //slidesPerGroup: 6,
+  spaceBetween: 30,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
---
+++ src/script.js
@@ -0,0 +1,7 @@
+const tswiper = new Swiper(".swiper", {
+  slidesPerView: 2,
+  spaceBetween: 10,
+  navigation: {
+    nextEl: ".swiper-button-next",
+  },
+});
